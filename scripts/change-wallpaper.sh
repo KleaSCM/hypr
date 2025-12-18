b747bd506b7b9b5ebd628ecf154276a7f8497dee
klea <klea@localhost>
2025-12-18 15:29:24 +0900

    feat: add change-wallpaper.sh script

---
+++ scripts/change-wallpaper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Pick a random wallpaper from ~/Pictures/wallpapers and set it with swww
+WALLPAPER=$(find "$HOME/Pictures/wallpapers" -type f | shuf -n 1)
+if [ -z "$WALLPAPER" ]; then
+  echo "No wallpapers found in $HOME/Pictures/wallpapers" >&2
+  exit 1
+fi
+
+swww img "$WALLPAPER" --transition-type fade
